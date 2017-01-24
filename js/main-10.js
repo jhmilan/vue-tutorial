ca6ef6a31938cddacb2c981d293a6b089fa63108
Jose H. Milan <jhmilan@gmail.com>
2017-01-24 12:21:22 +0100

    Lesson 10. Bulma component: modal

---
+++ js/main-10.js
@@ -0,0 +1,18 @@
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="box"><slot></slot></div>
+            </div>
+            <button class="modal-close" @click="$emit('close')"></button>
+        </div>
+    `
+});
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        showModal: false
+    }
+});
